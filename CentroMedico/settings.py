a6db4e4fcda656a39ba1d17b37fb39c9e207e544
tomaXx28 <oleax28@gmail.com>
2023-11-12 13:04:21 -0300

    crispy arreglado

--- CentroMedico/settings.py
+++ CentroMedico/settings.py
@@ -47,10 +47,13 @@ INSTALLED_APPS = [
     'app',
     'django.contrib.humanize',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 CRISPY_TEMPLATE_PACK= 'bootstrap5'
 
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 
 MIDDLEWARE = [
